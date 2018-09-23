697a126b904457ce61878e7eb57bf93222482249
Ramin Firooz <eleram.com@gmail.com>
2018-09-23 15:11:55 +0330

    Update persian-datepicker.js

--- dist/js/persian-datepicker.js
+++ dist/js/persian-datepicker.js
@@ -1938,7 +1938,7 @@ var Navigator = function () {
                 $(document).on('click', '#' + that.model.view.id + ' .up-btn', function () {
                     var timekey = $(this).data('time-key');
                     that.timeUp(timekey);
-                    that.model.options.onSelect(that.model.state.selected.unix);
+                    that.model.options.onSelect(that.model.state.selected.unixDate);
                 });
 
                 /**
@@ -1947,7 +1947,7 @@ var Navigator = function () {
                 $(document).on('click', '#' + that.model.view.id + ' .down-btn', function () {
                     var timekey = $(this).data('time-key');
                     that.timeDown(timekey);
-                    that.model.options.onSelect(that.model.state.selected.unix);
+                    that.model.options.onSelect(that.model.state.selected.unixDate);
                 });
             }
 
@@ -2004,7 +2004,7 @@ var Navigator = function () {
                     that.model.state.setViewDateTime('month', month);
                     that.model.view.render();
                     that.model.options.monthPicker.onSelect(month);
-                    that.model.options.onSelect(that.model.state.selected.unix);
+                    that.model.options.onSelect(that.model.state.selected.unixDate);
                 });
             }
 
@@ -2029,7 +2029,7 @@ var Navigator = function () {
                     that.model.state.setViewDateTime('year', year);
                     that.model.view.render();
                     that.model.options.yearPicker.onSelect(year);
-                    that.model.options.onSelect(that.model.state.selected.unix);
+                    that.model.options.onSelect(that.model.state.selected.unixDate);
                 });
             }
         }
